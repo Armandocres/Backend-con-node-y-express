64a09aca22b1eb393eef6a46eb0aa88cd681cc5c
Armandocres <64869199+Armandocres@users.noreply.github.com>
2021-10-09 14:39:59 -0500

    DEPLOY

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const routerAPi = require("./routes/index");
+const cors = require("cors");
 
 const {
   logErrors,
@@ -8,9 +9,23 @@ const {
 } = require("./middlewares/error.handler");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const whitelist = ["ruta"];
+
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("no permitido"));
+    }
+  }
+};
+
+app.use(cors(options));
 
 app.get("/", (req, res) => {
   res.send("Hola mi server en Express");
